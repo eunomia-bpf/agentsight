0a8eb05acb58f7d1e31eeff53606e247030f47fa
云微 <1067852565@qq.com>
2026-08-14 21:30:26 -0700

    refactor: move system aggregation to analysis ext

--- agentsight-capture/src/runners/mod.rs
+++ agentsight-capture/src/runners/mod.rs
@@ -6,13 +6,9 @@ use async_trait::async_trait;
 use futures::stream::Stream;
 use std::pin::Pin;
 
-/// Type alias for event streams
 pub type EventStream = Pin<Box<dyn Stream<Item = Event> + Send>>;
-
-/// Type alias for errors that can be sent between threads
 pub type RunnerError = Box<dyn std::error::Error + Send + Sync>;
 
-/// Base trait for all runners that collect observability data
 #[async_trait]
 pub trait Runner: Send + Sync {
     async fn run(&mut self) -> Result<EventStream, RunnerError>;
@@ -27,11 +23,9 @@ pub mod common;
 #[cfg(any(test, feature = "test-support"))]
 pub mod fake;
 pub mod process;
-pub mod system;
 
 pub use agent::AgentRunner;
 pub use common::BinaryRunner;
 #[cfg(any(test, feature = "test-support"))]
 pub use fake::FakeRunner;
 pub use process::ProcessRunner;
-pub use system::SystemRunner;
---
+++ ext/analysis/src/runners/mod.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 eunomia-bpf org.
+
+pub use agentsight_capture_core::runners::{
+    AgentRunner, BinaryRunner, EventStream, ProcessRunner, Runner, RunnerError,
+};
+#[cfg(any(test, feature = "test-support"))]
+pub use agentsight_capture_core::runners::FakeRunner;
+
+mod system;
+pub use system::{SystemConfig, SystemRunner};
